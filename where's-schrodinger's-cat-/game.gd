6fcad6a7e0f5cc1e728867dc27d05b3047368cba
augustgrantham <august.grantham@gmail.com>
2026-02-21 09:18:54 -0600

    Add basic gameloop function

--- where's-schrodinger's-cat-/game.gd
+++ where's-schrodinger's-cat-/game.gd
@@ -2,17 +2,20 @@ extends Node2D
 
 var box_scene = preload("res://Box.tscn")
 var points = 0
-var boxWithCat = randi_range(1,3)
+var boxWithCat = 0
+@onready var winStatuslabel = $CanvasLayer/winLabel
+@onready var scoreLabel = $CanvasLayer/scoreLabel
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
+	startRound()
 
-	
-	print(boxWithCat)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
 
-
+func startRound():
+	boxWithCat = randi_range(1,3)
+	print(boxWithCat)
 func _on_box_1_box_clicked(id: Variant) -> void:
 	boxClicked(1)
 
@@ -24,6 +27,15 @@ func _on_box_3_box_clicked(id: Variant) -> void:
 
 func boxClicked(id):
 	if (id == boxWithCat):
+		winStatuslabel.text = "Nice Job!"
+		points += 1
+		scoreLabel.text = str(points)
 		print("yipee!!")
+		await get_tree().create_timer(2.0).timeout
+		winStatuslabel.text = ""
+		startRound()
 	else:
+		winStatuslabel.text = "Nice Try!"
+		await get_tree().create_timer(2.0).timeout
 		print("you lost dummy")
+		get_tree().change_scene_to_file("res://mainmenu.tscn")
